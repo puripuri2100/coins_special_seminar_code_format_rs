344bf80d5c0275b376435bc93f232ac574e4df5a
puripuri2100 <puripuri2100@gmail.com>
2022-12-15 14:28:51 +0900

    update

--- src/tree.rs
+++ src/tree.rs
@@ -2,7 +2,14 @@ mod format;
 
 use crate::{ColumnConfig, Context};
 
+/// コードの形態を4つに分類した。
 /// 
+/// - リテラル
+/// - 同じ階層のトークン列
+/// - 括弧
+/// - 途中で改行を行ったりできるトークン列
+/// 
+/// これをそれぞれRaw・List・Paren・Columnとして名前を付けた。
 #[derive(Clone, Debug)]
 pub enum Rule {
   AST(Box<RuleWithComment>),
@@ -12,7 +19,9 @@ pub enum Rule {
   Column(Vec<(RuleWithComment, ColumnConfig)>),
 }
 
-/// コメントはそのトークンの出現する前の
+/// コメントはそのトークンの前に出現するものと、そのトークンの直後に出現するものにわかれる。
+/// トークンの前に出現するコメントは複数行が存在するため、リストで管理する。
+/// トークンの直後に出現するコメントは「存在する」か「存在しない」なのでOptionで管理する
 #[derive(Clone, Debug)]
 pub struct RuleWithComment {
   pub before_comments: Vec<String>,
